88d813b93d601b522ac08f1a94ae1b9da38e5cfb
Lisa Lee <snickers495@gmail.com>
2017-10-24 19:58:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = rake db:create_migration NAME=create_movies
   movie.title = "This is a title."
   __
 end
